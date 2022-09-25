0020bffb40dc3b57cc1d36733adc4e832441db8e
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-25 16:47:46 +0200

    created service user

---
+++ src/main/java/net/parwand/springregister/applicationservice/service/UserService.java
@@ -0,0 +1,23 @@
+package net.parwand.springregister.applicationservice.service;
+
+import net.parwand.springregister.applicationservice.UserRepository;
+import net.parwand.springregister.domain.model.user.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public User getUser(String username) {
+        return repository.findUserByUsername(username);
+    }
+
+    public void save(User user) {
+        repository.save(user);
+    }
+}
