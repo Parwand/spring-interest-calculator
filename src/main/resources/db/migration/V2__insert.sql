b751dc996e4321e067d3c840ec6550b470320166
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-25 15:30:27 +0200

    set a new password for student

--- src/main/resources/db/migration/V2__insert.sql
+++ src/main/resources/db/migration/V2__insert.sql
@@ -1,3 +1,3 @@
 INSERT INTO user(username, password, role) VALUES ( 'admin', '$2a$12$QVHl7ywUJ4LhDEranx9t6OSosRKApI8v.VqMr7AwoSkVq/fwIGlHm', 'ROLE_ADMIN');
-INSERT INTO user(username, password, role) VALUES ( 'student', '$2a$12$aWE1NXoUqZNGPfO9LzAGPuNhpRX96OJa01vjI6st/EakyUwAxrvH6', 'ROLE_STUDENT');
+INSERT INTO user(username, password, role) VALUES ( 'student', '$2a$12$9m051.N/GGXZjeJs6Jrai.1ONtXeUjoLhvz4xY2jzmjOxg4fefZmi', 'ROLE_STUDENT');
 INSERT INTO user(username, password, role) VALUES ( 'user', '$2a$12$jrkU3gf5PX.EV0NqutCvBOqPNOflr6AJ.NAPBURESEP.Xce4/moPK', 'ROLE_USER');
