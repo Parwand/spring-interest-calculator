764ec495dd3781c80f1a4d01598c01a14798178d
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-26 00:53:25 +0200

    mocked the bean of clientRegistrationRepository

--- src/test/java/net/parwand/springregister/infrastructure/controller/ControllerTest.java
+++ src/test/java/net/parwand/springregister/infrastructure/controller/ControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
@@ -25,6 +27,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class ControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @MockBean
+    ClientRegistrationRepository clientRegistrationRepository;
 
     /**
      * TODO: Testing with Global Method Security.
