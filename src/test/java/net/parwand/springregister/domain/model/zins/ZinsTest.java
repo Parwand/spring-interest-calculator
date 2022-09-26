9dfb9ba3a9fecbb85b7a46147caecce8fc532f1f
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-26 09:25:23 +0200

    cretated unit-test for Zins-Model

--- src/test/java/net/parwand/springregister/domain/model/zins/ZinsTest.java
+++ src/test/java/net/parwand/springregister/domain/model/zins/ZinsTest.java
@@ -1,4 +1,14 @@
+package net.parwand.springregister.domain.model.zins;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ZinsTest {
-  
+
+    @Test
+    @DisplayName("")
+    void test_1() {
+    }
 }
