4386baaeb2edc3b6b99f4c426ee81040223ea361
Parwand Alsino <parwand.alsino@hhu.de>
2022-09-26 11:39:44 +0200

    added a new test_2

--- src/test/java/net/parwand/springregister/domain/model/zins/ZinsTest.java
+++ src/test/java/net/parwand/springregister/domain/model/zins/ZinsTest.java
@@ -41,4 +41,19 @@ class ZinsTest {
         // Assert
         assertThat(result).isEqualTo(275.63);
     }
+
+    @Test
+    @DisplayName("Mit Anfangskapital 0 €, 0 Jahren Laufzeit  und 10 % Zinssatz soll Endkapital 0€ betragen ")
+    void test_3() {
+        // Arrange
+        Zins zins = new Zins();
+        zins.setAnfangskapital(0);
+        zins.setLaufzeit(0);
+        zins.setZinssatz(10);
+        // Act
+        zins.zinsenBerechnen();
+        double result = zins.getEndkapital();
+        // Assert
+        assertThat(result).isEqualTo(0.0);
+    }
 }
